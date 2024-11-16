c13c6673cecae37bacab3af8d8a6817d843b3014
SputnikPro <averina@sputnik.llc>
2024-11-16 21:13:35 +0300

    source

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/SputnikPro_test/', 
+  build: {
+		outDir: 'dist',
+		rollupOptions: {
+			input: {
+				index: resolve(__dirname, "index.html"),
+				action: resolve(__dirname, "src/html/action.html"),
+				parameters: resolve(__dirname, "src/html/parameters.html"),
+			}
+		}
+	}
 });
